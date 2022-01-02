25340bb32aa8096f82050f2f3bb1bf1d06afda83
Lomatskiy Mikhail <58404502+mikhail-lom@users.noreply.github.com>
2022-01-02 17:01:02 +0300

    Exercise 1 result code

---
+++ src/Chapter3/ExercisesForSelfControl/Exercise1.java
@@ -0,0 +1,18 @@
+package Chapter3.ExercisesForSelfControl;
+
+/**
+ * Exercise 1 from chapter 3
+ */
+public class Exercise1 {
+    public static void main(String[] args) throws java.io.IOException {
+        char userInputValue;
+        int spaceCounter = 0;
+        System.out.println("Enter any button (for stop press '.')");
+        do {
+            userInputValue = (char) System.in.read();
+            if (userInputValue == ' ') spaceCounter++;
+        } while (userInputValue != '.');
+        System.out.println("Spaces: " + spaceCounter);
+
+    }
+}
