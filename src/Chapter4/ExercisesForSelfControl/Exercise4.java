4b705199c061c869a536bdf729c060e198c704fd
Lomatskiy Mikhail <58404502+mikhail-lom@users.noreply.github.com>
2022-01-03 11:50:12 +0300

    Exercises 4 and 5 chapter 4

---
+++ src/Chapter4/ExercisesForSelfControl/Exercise4.java
@@ -0,0 +1,11 @@
+package Chapter4.ExercisesForSelfControl;
+
+public class Exercise4 {
+    public static void main(String[] args) {
+        class MyCounter{
+
+        }
+        MyCounter counter;
+        counter= new MyCounter();
+    }
+}
---
+++ src/Chapter4/ExercisesForSelfControl/Exercise5.java
@@ -0,0 +1,19 @@
+package Chapter4.ExercisesForSelfControl;
+
+public class Exercise5 {
+    public static void main(String[] args) {
+
+        double testVar;
+
+
+        Exercise5 object = new Exercise5();
+        testVar = object.myMeth(6,8);
+
+    }
+    double myMeth(int a, int b) {
+        System.out.println(a);
+        System.out.println(b);
+        return 0;
+    }
+}
+
