902aa46d8cffed65fd7f7f5971d207fe23d9f68c
Lomatskiy Mikhail <58404502+mikhail-lom@users.noreply.github.com>
2022-01-03 10:04:31 +0300

    Exercise 9 chapter 3 result from the book

---
+++ src/Chapter3/ExercisesForSelfControl/Exercise9.java
@@ -0,0 +1,11 @@
+package Chapter3.ExercisesForSelfControl;
+
+/**
+ * Exercise 9 from chapter 3
+ * geometrical progression
+ */
+public class Exercise9 {
+    public static void main(String[] args){
+        for (int i = 1; i < 100; i+=i) System.out.print(i + " ");
+    }
+}
