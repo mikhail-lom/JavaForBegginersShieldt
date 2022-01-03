d32ba20e003cdf3204ef7edf2db8ad0ee8343859
Lomatskiy Mikhail <58404502+mikhail-lom@users.noreply.github.com>
2022-01-03 20:50:13 +0300

    Exercise 9 chapter5

---
+++ src/Chapter5/ExercisesForSelfControl/Exercise9.java
@@ -0,0 +1,10 @@
+package Chapter5.ExercisesForSelfControl;
+
+public class Exercise9 {
+    public static void main(String[] args) {
+        int x = -9, y;
+
+        y= x < 0 ?  10 : 20;
+        System.out.println(y);
+    }
+}
