c2e6251c3c9d48bda97e691736873606258c8ae7
Lomatskiy Mikhail <58404502+mikhail-lom@users.noreply.github.com>
2022-03-18 15:20:16 +0300

    Simple quadratic equation solver

--- src/chapter12/Quadratic.java
+++ src/chapter12/Quadratic.java
@@ -3,22 +3,28 @@ import static java.lang.Math.pow;
 import static java.lang.Math.sqrt;
 
 public class Quadratic {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
         //a,b,c coefficients of
         //quadratic equation ax^2 + bx + c = 0
-        double a, b, c, x;
+       double a, b, c, x, d;
 
         //Solve quadratic equation 4x^2 + x - 3 = 0
         a = 4;
         b = 1;
         c = -3;
 
+        //Discriminant finding
+        d = sqrt(pow(b, 2) - 4 * a * c);
+
         //Find first root
-        x = (-b + sqrt(pow(b, 2) - 4 * a * c)) / (2 * a);
+        x = (-b + d) / (2 * a);
         System.out.println("First root is: " + x);
 
+
         //Find second root
-        x =
+        x = (-b - d) / (2 * a);
+        System.out.println("Second root is: " + x);
+
     }
 }
