6e4aca9e0c4bf8ce356e7954314ec65b041bd04d
Lomatskiy Mikhail <58404502+mikhail-lom@users.noreply.github.com>
2022-03-21 09:48:01 +0300

    Book variant of Tools enum exercise

--- src/chapter12/self_check_exercises/ToolUsage.java
+++ src/chapter12/self_check_exercises/ToolUsage.java
@@ -2,11 +2,9 @@ package chapter12.self_check_exercises;
 
 public class ToolUsage {
     public static void main(String[] args) {
-        Tools[] toolArr = Tools.values();
-        for (int i = 0; i < toolArr.length; i++) {
-            System.out.println(toolArr[i]);
+        for (Tools t :
+                Tools.values()) {
+            System.out.println(t);
         }
-
-
     }
 }
