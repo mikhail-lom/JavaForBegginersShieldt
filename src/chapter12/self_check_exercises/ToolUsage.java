297cae81d36ce05d261d6458ccd4036396780d99
Lomatskiy Mikhail <58404502+mikhail-lom@users.noreply.github.com>
2022-03-21 09:46:08 +0300

    My variant of enum Tools exercise

---
+++ src/chapter12/self_check_exercises/ToolUsage.java
@@ -0,0 +1,12 @@
+package chapter12.self_check_exercises;
+
+public class ToolUsage {
+    public static void main(String[] args) {
+        Tools[] toolArr = Tools.values();
+        for (int i = 0; i < toolArr.length; i++) {
+            System.out.println(toolArr[i]);
+        }
+
+
+    }
+}
---
+++ src/chapter12/self_check_exercises/Tools.java
@@ -0,0 +1,5 @@
+package chapter12.self_check_exercises;
+
+public enum Tools {
+    SCREWDRIVER, WRENCH, HAMMER, PLIERS
+}
