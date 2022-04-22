28ba510e95ee1143894906979e219c59352d85d7
Lomatskiy Mikhail <58404502+mikhail-lom@users.noreply.github.com>
2022-04-22 08:56:51 +0300

    Qucksort with objective model

---
+++ src/algorithms/QuickSortObjective.java
@@ -0,0 +1,22 @@
+package algorithms;
+
+public class QuickSortObjective {
+    QuickSortObjective lowArray;
+    int pivot;
+    QuickSortObjective highArray;
+
+    //Constructors
+    QuickSortObjective(){
+
+    }
+
+    QuickSortObjective(QuickSortObjective l, int p, QuickSortObjective h){
+        lowArray = l;
+        pivot = p;
+        highArray = h;
+    }
+
+    void sort(){
+
+    }
+}
